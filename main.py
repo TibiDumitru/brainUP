03ba97db8437eac4d3ce8b3467631d4be4db6f85
paulbenge <63068592+paulbenge@users.noreply.github.com>
2020-04-04 16:41:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import re
 
 app = Flask(__name__)
 
-# Change this to your secret key (can be anything, it's for extra protection)
-app.secret_key = 'nimeni nu stie'
+
+app.secret_key = 'vinti_fara_restante'
 
 # Enter your database connection details below
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'parola10'
-app.config['MYSQL_DB'] = 'pythonlogin'
+app.config['MYSQL_HOST'] = 'sql7.freemysqlhosting.net'
+app.config['MYSQL_USER'] = 'sql7330808'
+app.config['MYSQL_PASSWORD'] = 'i8AwCUhJaT'
+app.config['MYSQL_DB'] = 'sql7330808'
 
 # Intialize MySQL
 mysql = MySQL(app)
@@ -65,11 +65,14 @@ def register():
     # Output message if something goes wrong...
     msg = ''
     # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'name' in request.form and 'category' in request.form:
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
+        name = request.form['name']
+        category = request.form['category']
+
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
@@ -81,11 +84,13 @@ def register():
             msg = 'Invalid email address!'
         elif not re.match(r'[A-Za-z0-9]+', username):
             msg = 'Username must contain only characters and numbers!'
-        elif not username or not password or not email:
+        elif not re.match(r'[A-Za-z]+', name):
+            msg = 'Name must contain only characters!'
+        elif not username or not password or not email or not name or not category:
             msg = 'Please fill out the form!'
         else:
             # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s, %s)', (username, password, email, name, category,))
             mysql.connection.commit()
             msg = 'You have successfully registered!'
     elif request.method == 'POST':
